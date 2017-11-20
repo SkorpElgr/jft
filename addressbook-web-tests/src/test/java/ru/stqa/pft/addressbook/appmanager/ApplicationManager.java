2cfc2df0558c65e89686cf0e6c3d117976879190
pskorodzievskiy <paul.skorodzievskiy@gmail.com>
2017-11-20 23:31:43 +0200

    TestConfigs placed to properties file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/ApplicationManager.java
@@ -8,12 +8,17 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.remote.BrowserType;
 
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Properties;
 import java.util.concurrent.TimeUnit;
 
 /**
  * Created by Admin on 22.10.2017.
  */
 public class ApplicationManager {
+    private final Properties properties;
     WebDriver wd;
     private NavigationHelper navigationHelper;
     private GroupHelper groupHelper;
@@ -23,14 +28,17 @@ public class ApplicationManager {
 
     public ApplicationManager(String browser) {
         this.browser = browser;
+        properties = new Properties();
     }
 
-    public void init() {
-        if (browser == BrowserType.FIREFOX) {
+    public void init() throws IOException {
+        String target = System.getProperty("target", "local");
+        properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties", target))));
+        if (browser.equals(BrowserType.FIREFOX)) {
             wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true));
-        } else if (browser == BrowserType.CHROME) {
+        } else if (browser.equals(BrowserType.CHROME)) {
             wd = new ChromeDriver();
-        } else if (browser == BrowserType.IE) {
+        } else if (browser.equals(BrowserType.IE)) {
             System.setProperty("webdriver.ie.driver", "src/drivers/IEDriverServer.exe");
             InternetExplorerOptions options = new InternetExplorerOptions();
             options.introduceFlakinessByIgnoringSecurityDomains();
@@ -39,11 +47,12 @@ public class ApplicationManager {
         }
         // wd = new FirefoxDriver(new FirefoxOptions().setLegacy(true).setBinary("C:/Program Files/Mozilla Firefox ESR/firefox.exe"));
         wd.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
+        wd.get(properties.getProperty("web.baseUrl"));
         groupHelper = new GroupHelper(wd);
         navigationHelper = new NavigationHelper(wd);
         navigationHelper.goToMainPage();
         sessionHelper = new SessionHelper(wd);
-        sessionHelper.login("secret", "admin");
+        sessionHelper.login(properties.getProperty("web.adminPassword"), properties.getProperty("web.adminLogin"));
         contactHelper = new ContactHelper(wd);
     }
 
